ed8fb29a583bac2a9a1dc905452dc3f1ad2c4edf
Gavinboley <89856082+Gavinboley@users.noreply.github.com>
2022-09-29 07:09:34 -0500

    Update ClientSend.cs

--- Scripts(From Unity Projects Folder)/FPS/ClientSend.cs
+++ Scripts(From Unity Projects Folder)/FPS/ClientSend.cs
@@ -1,19 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ClientSend : MonoBehaviour
 {
-    /// <summary>Sends a packet to the server via TCP.</summary>
-    /// <param name="_packet">The packet to send to the sever.</param>
     private static void SendTCPData(Packet _packet)
     {
         _packet.WriteLength();
         Client.instance.tcp.SendData(_packet);
     }
-
-    /// <summary>Sends a packet to the server via UDP.</summary>
-    /// <param name="_packet">The packet to send to the sever.</param>
+    
     private static void SendUDPData(Packet _packet)
     {
         _packet.WriteLength();
@@ -21,7 +17,6 @@ public class ClientSend : MonoBehaviour
     }
 
     #region Packets
-    /// <summary>Lets the server know that the welcome message was received.</summary>
     public static void WelcomeReceived()
     {
         using (Packet _packet = new Packet((int)ClientPackets.welcomeReceived))
@@ -33,8 +28,6 @@ public class ClientSend : MonoBehaviour
         }
     }
 
-    /// <summary>Sends player input to the server.</summary>
-    /// <param name="_inputs"></param>
     public static void PlayerMovement(bool[] _inputs)
     {
         using (Packet _packet = new Packet((int)ClientPackets.playerMovement))
