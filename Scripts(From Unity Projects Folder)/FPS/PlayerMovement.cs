5186d867b6e3c9e6e1e7da2662687ab71a13ab40
Gavinboley <89856082+Gavinboley@users.noreply.github.com>
2022-09-29 07:16:48 -0500

    Update PlayerMovement.cs

--- Scripts(From Unity Projects Folder)/FPS/PlayerMovement.cs
+++ Scripts(From Unity Projects Folder)/FPS/PlayerMovement.cs
@@ -18,7 +18,6 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
-    // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
